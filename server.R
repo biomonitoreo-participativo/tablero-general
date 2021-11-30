df645e76bc0722f4c9d1d27e7e066d9776852cd9
mfvargas <mfvargas@gmail.com>
2021-11-30 11:24:55 -0600

    Agregar páginas separadas para mapa, tabla y gráficos de registros de presencia

--- server.R
+++ server.R
@@ -2,14 +2,14 @@ shinyServer(function(input, output, session) {
     # filtrarAreasConservacion <- reactive({
     #     areas_conservacion_filtradas <-
     #         areas_conservacion
-    #     
+    #
     #     # Filtrado por área de conservación
     #     if (input$selector_areas_conservacion != "Todas") {
     #         areas_conservacion_filtradas <-
     #             areas_conservacion_filtradas %>%
-    #             filter(siglas_ac == input$selector_areas_conservacion)            
-    #     }        
-    #     
+    #             filter(siglas_ac == input$selector_areas_conservacion)
+    #     }
+    #
     #     return (areas_conservacion_filtradas)
     # })
     
@@ -60,7 +60,7 @@ shinyServer(function(input, output, session) {
                 label = "Especies indicadoras",
                 choices = lista_especies_grupo,
                 selected = "Todas"
-            )            
+            )
         }
         
         # Filtrado por especie
@@ -78,7 +78,7 @@ shinyServer(function(input, output, session) {
             
             areas_conservacion <-
                 areas_conservacion %>%
-                filter(siglas_ac == input$selector_areas_conservacion)            
+                filter(siglas_ac == input$selector_areas_conservacion)
         }
         
         # Filtrado por corredor biológico
@@ -93,7 +93,7 @@ shinyServer(function(input, output, session) {
             registros_presencia_filtrados <-
                 registros_presencia_filtrados %>%
                 filter(fuente == input$selector_fuentes_datos)
-        }        
+        }
         
         return(registros_presencia_filtrados)
     })
@@ -243,12 +243,14 @@ shinyServer(function(input, output, session) {
         
         registros_presencia_filtrados %>%
             st_drop_geometry() %>%
-            select(scientificName,
-                   locality,
-                   eventDate,
-                   corredor_biologico,
-                   area_conservacion,
-                   fuente) %>%
+            select(
+                scientificName,
+                locality,
+                eventDate,
+                corredor_biologico,
+                area_conservacion,
+                fuente
+            ) %>%
             datatable(
                 rownames = FALSE,
                 colnames = c("Especie", "Localidad", "Fecha", "CB", "AC", "Fuente"),
@@ -257,7 +259,7 @@ shinyServer(function(input, output, session) {
                     pageLength = 5,
                     searchHighlight = TRUE,
                     lengthMenu = list(
-                        c(5, 10, 15, 25, 50, 100,-1),
+                        c(5, 10, 15, 25, 50, 100, -1),
                         c(5, 10, 15, 25, 50, 100, "Todos")
                     ),
                     dom = 'Bfrtlip',
@@ -272,31 +274,243 @@ shinyServer(function(input, output, session) {
             )
     })
     
-    output$grafico_corredores_biologicos_especies_resumen <- renderPlotly({
-        corredores_biologicos_filtrados <- filtrarCorredoresBiologicos()
+    output$grafico_corredores_biologicos_especies_resumen <-
+        renderPlotly({
+            corredores_biologicos_filtrados <- filtrarCorredoresBiologicos()
+            
+            corredores_biologicos_filtrados %>%
+                st_drop_geometry() %>%
+                filter(cantidad_especies >= 1) %>%
+                top_n(n = 20, wt = cantidad_especies) %>%
+                mutate(nombre_cb = factor(nombre_cb, levels = unique(nombre_cb)[order(cantidad_especies, decreasing = TRUE)])) %>%
+                arrange(desc(cantidad_especies)) %>%
+                plot_ly(
+                    x = ~ nombre_cb,
+                    y = ~ cantidad_especies,
+                    type = "bar",
+                    name = "Proyectos",
+                    text = ~ cantidad_especies,
+                    textposition = 'auto',
+                    marker = list(color = "#2c7fb8")
+                ) %>%
+                layout(
+                    yaxis = list(title = "Cantidad de especies"),
+                    xaxis = list(title = "Corredor biológico"),
+                    barmode = 'group',
+                    hovermode = "compare"
+                ) %>%
+                config(locale = 'es')
+        })
+    
+    output$mapa_registros_presencia <- renderLeaflet({
+        # areas_conservacion_filtradas <- filtrarAreasConservacion()
+        registros_presencia_filtrados <- filtrarRegistrosPresencia()
+        corredores_biologicos_filtrados <-
+            filtrarCorredoresBiologicos()
         
-        corredores_biologicos_filtrados %>%
-            st_drop_geometry() %>%
-            filter(cantidad_especies >= 1) %>%
-            top_n(n = 20, wt = cantidad_especies) %>%
-            mutate(nombre_cb = factor(nombre_cb, levels = unique(nombre_cb)[order(cantidad_especies, decreasing = TRUE)])) %>%
-            arrange(desc(cantidad_especies)) %>%
-            plot_ly(
-                x = ~ nombre_cb,
-                y = ~ cantidad_especies,
-                type = "bar",
-                name = "Proyectos",
-                text = ~ cantidad_especies,
-                textposition = 'auto',
-                marker = list(color = "#2c7fb8")
+        pal_corredores_biologicos_especies <-
+            colorNumeric(palette = "YlGnBu",
+                         domain = corredores_biologicos_filtrados$cantidad_especies)
+        
+        # Mapa Leaflet con capas de ...
+        leaflet() %>%
+            addTiles(group = "OpenStreetMap") %>%
+            addProviderTiles(providers$Stamen.TonerLite, group = "Stamen Toner Lite") %>%
+            addProviderTiles(providers$CartoDB.DarkMatter, group = "CartoDB Dark Matter") %>%
+            addProviderTiles(providers$Esri.WorldImagery, group = "Imágenes de ESRI") %>%
+            addPolygons(
+                data = areas_conservacion,
+                group = "Áreas de conservación",
+                color = "brown",
+                stroke = TRUE,
+                weight = 4.0,
+                fillOpacity = 0.0,
+                popup = paste0(
+                    "<strong>Área de conservación: </strong>",
+                    areas_conservacion$nombre_ac
+                )
             ) %>%
-            layout(
-                yaxis = list(title = "Cantidad de especies"),
-                xaxis = list(title = "Corredor biológico"),
-                barmode = 'group',
-                hovermode = "compare"
+            addPolygons(
+                data = corredores_biologicos_filtrados,
+                group = "Corredores biológicos",
+                color = "green",
+                stroke = TRUE,
+                fillColor = ~ pal_corredores_biologicos_especies(cantidad_especies),
+                weight = 2.0,
+                fillOpacity = 0.8,
+                popup = paste0(
+                    "<strong>Corredor biológico: </strong>",
+                    corredores_biologicos_filtrados$nombre_cb,
+                    "<br>",
+                    "<strong>Cantidad de especies: </strong>",
+                    corredores_biologicos_filtrados$cantidad_especies
+                )
             ) %>%
-            config(locale = 'es')        
+            addLegend(
+                position = "bottomright",
+                pal = pal_corredores_biologicos_especies,
+                values = corredores_biologicos_filtrados$cantidad_especies,
+                labFormat = labelFormat(digits = 0),
+                group = "Corredores biológicos",
+                title = "Cantidad de especies"
+            ) %>%
+            addCircleMarkers(
+                data = registros_presencia_filtrados,
+                group = "Registros de presencia",
+                stroke = TRUE,
+                radius = 4,
+                fillColor = 'red',
+                fillOpacity = 1,
+                label = paste0(
+                    registros_presencia_filtrados$scientificName,
+                    ", ",
+                    registros_presencia_filtrados$locality,
+                    ", ",
+                    registros_presencia_filtrados$eventDate
+                ),
+                popup = paste0(
+                    "<strong>Especie: </strong>",
+                    registros_presencia_filtrados$scientificName,
+                    "<br>",
+                    "<strong>Localidad: </strong>",
+                    registros_presencia_filtrados$locality,
+                    "<br>",
+                    "<strong>Fecha: </strong>",
+                    registros_presencia_filtrados$eventDate,
+                    "<br>",
+                    "<strong>Área de conservación: </strong>",
+                    registros_presencia_filtrados$area_conservacion,
+                    "<br>",
+                    "<strong>Corredor biológico: </strong>",
+                    registros_presencia_filtrados$corredor_biologico,
+                    "<br>",
+                    "<strong>Fuente de los datos: </strong>",
+                    registros_presencia_filtrados$fuente
+                )
+            ) %>%
+            addLayersControl(
+                baseGroups = c(
+                    "OpenStreetMap",
+                    "Stamen Toner Lite",
+                    "CartoDB Dark Matter",
+                    "Imágenes de ESRI"
+                ),
+                overlayGroups = c(
+                    "Áreas de conservación",
+                    "Corredores biológicos",
+                    "Registros de presencia"
+                )
+            ) %>%
+            addScaleBar(position = "bottomleft",
+                        options = scaleBarOptions(imperial = FALSE)) %>%
+            addMouseCoordinates() %>%
+            addSearchOSM() %>%
+            addResetMapButton()
     })
     
+    output$tabla_registros_presencia <- renderDT({
+        registros_presencia_filtrados <- filtrarRegistrosPresencia()
+        
+        registros_presencia_filtrados %>%
+            st_drop_geometry() %>%
+            select(
+                grupos_nomenclaturales,
+                scientificName,
+                locality,
+                eventDate,
+                corredor_biologico,
+                area_conservacion,
+                fuente
+            ) %>%
+            datatable(
+                rownames = FALSE,
+                colnames = c(
+                    "Grupo",
+                    "Especie",
+                    "Localidad",
+                    "Fecha",
+                    "Corredor biológico",
+                    "Área de conservación",
+                    "Fuente"
+                ),
+                extensions = c("Buttons"),
+                options = list(
+                    pageLength = 10,
+                    searchHighlight = TRUE,
+                    lengthMenu = list(
+                        c(10, 20, 30, 40, 50, 100, -1),
+                        c(10, 20, 30, 40, 50, 100, "Todos")
+                    ),
+                    dom = 'Bfrtlip',
+                    language = list(url = "//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json"),
+                    buttons = list(
+                        list(extend = 'copy', text = 'Copiar'),
+                        list(extend = 'csv', text = 'CSV'),
+                        list(extend = 'csv', text = 'Excel'),
+                        list(extend = 'pdf', text = 'PDF')
+                    )
+                )
+            )
+    })
+    
+    output$grafico_corredores_biologicos_especies <-
+        renderPlotly({
+            corredores_biologicos_filtrados <- filtrarCorredoresBiologicos()
+            
+            corredores_biologicos_filtrados %>%
+                st_drop_geometry() %>%
+                filter(cantidad_especies >= 1) %>%
+                top_n(n = 20, wt = cantidad_especies) %>%
+                mutate(nombre_cb = factor(nombre_cb, levels = unique(nombre_cb)[order(cantidad_especies, decreasing = TRUE)])) %>%
+                arrange(desc(cantidad_especies)) %>%
+                plot_ly(
+                    x = ~ nombre_cb,
+                    y = ~ cantidad_especies,
+                    type = "bar",
+                    name = "Proyectos",
+                    text = ~ cantidad_especies,
+                    textposition = 'auto',
+                    marker = list(color = "#2c7fb8")
+                ) %>%
+                layout(
+                    yaxis = list(title = "Cantidad de especies"),
+                    xaxis = list(title = "Corredor biológico"),
+                    barmode = 'group',
+                    hovermode = "compare"
+                ) %>%
+                config(locale = 'es')
+        })    
+    
+    output$grafico_estacionalidad_registros <-
+        renderPlotly({
+            registros_presencia_filtrados <- filtrarRegistrosPresencia()
+            
+            registros_presencia_filtrados %>%
+                st_drop_geometry() %>%
+                group_by(mes = format(as.Date(eventDate, "%Y-%m-%d"), "%m")) %>%
+                summarize(suma_registros = n()) %>%
+                filter(!is.na(mes))  %>%
+                plot_ly(x = ~ mes,
+                        y = ~ suma_registros,
+                        type="bar", color = "green") %>%
+                layout(xaxis = list(title = "Mes"),
+                       yaxis = list(title = "Cantidad de registros de presencia"))
+        })      
+  
+    output$grafico_estacionalidad_especies <-
+        renderPlotly({
+            registros_presencia_filtrados <- filtrarRegistrosPresencia()
+            
+            registros_presencia_filtrados %>%
+                st_drop_geometry() %>%
+                group_by(mes = format(as.Date(eventDate, "%Y-%m-%d"), "%m")) %>%
+                summarize(suma_especies = n_distinct(scientificName, na.rm = TRUE)) %>%
+                filter(!is.na(mes))  %>%
+                plot_ly(x = ~ mes,
+                        y = ~ suma_especies,
+                        type="bar", color = "green") %>%
+                layout(xaxis = list(title = "Mes"),
+                       yaxis = list(title = "Cantidad de especies"))
+        })          
+      
 })
--- ui.R
+++ ui.R
@@ -28,9 +28,12 @@ dashboardPage(
                 inputId = "selector_fuentes_datos",
                 label = "Fuente de datos",
                 choices = opciones_fuentes_datos
-            ),            
+            ),
             startExpanded = TRUE,
-            menuSubItem(text = "Resumen", tabName = "tab_resumen")
+            menuSubItem(text = "Resumen", tabName = "tab_resumen"),
+            menuSubItem(text = "Mapa registros presencia", tabName = "tab_mapa_registros_presencia"),
+            menuSubItem(text = "Tabla registros presencia", tabName = "tab_tabla_registros_presencia"),
+            menuSubItem(text = "Gráficos registros presencia", tabName = "tab_graficos_registros_presencia")
         )
     )),
     dashboardBody(tabItems(
@@ -59,6 +62,55 @@ dashboardPage(
                         plotlyOutput(outputId = "grafico_corredores_biologicos_especies_resumen", height = 200),
                         width = NULL
                     )
-                )))
+                ))),
+        tabItem(
+            tabName = "tab_mapa_registros_presencia",
+            fluidRow(column(
+                width = 12,
+                box(
+                    title = "Registros de presencia de especies indicadoras",
+                    leafletOutput(outputId = "mapa_registros_presencia", height = 700),
+                    width = NULL
+                )
+            ))
+        ),
+        tabItem(
+            tabName = "tab_tabla_registros_presencia",
+            fluidRow(column(
+                width = 12,
+                box(
+                    title = "Registros de presencia de especies indicadoras",
+                    DTOutput(outputId = "tabla_registros_presencia"),
+                    width = NULL
+                )
+            ))
+        ),
+        tabItem(
+            tabName = "tab_graficos_registros_presencia",
+            fluidRow(column(
+                width = 12,
+                box(
+                    title = "Cantidad de especies indicadoras en corredores biológicos",
+                    plotlyOutput(outputId = "grafico_corredores_biologicos_especies", height = 200),
+                    width = NULL
+                )
+            )),
+            fluidRow(column(
+                width = 12,
+                box(
+                    title = "Estacionalidad (registros de presencia)",
+                    plotlyOutput(outputId = "grafico_estacionalidad_registros", height = 200),
+                    width = NULL
+                )
+            )),
+            fluidRow(column(
+                width = 12,
+                box(
+                    title = "Estacionalidad (especies)",
+                    plotlyOutput(outputId = "grafico_estacionalidad_especies", height = 200),
+                    width = NULL
+                )
+            ))
+        )
     ))
 )
